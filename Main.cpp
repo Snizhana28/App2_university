d91a05d76477dff2c631502ac749f3158dbfc520
snizhana28 <snizhana2812kostyuk@gmail.com>
2024-01-20 21:24:55 +0200

    Done task 11, section 7

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string text = "¿À√Œ–»“Ã";
+
+	reverse(text.begin(), text.end());
+	cout << text << endl;
 
 	return 0;
 }

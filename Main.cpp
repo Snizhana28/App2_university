93804f28df9cebdd6e70c2e2ccd658809bd7db64
Snizhana Kostiuk <snizhana2812kostyuk@gmail.com>
2024-02-03 16:55:43 +0200

    Done task 9, section 11

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+	int n;
+	cout << "Enter the number of elements in the array: ";
+	cin >> n;
+	vector<int> A(n);
+	
+	cout << "Enter the element of the array: ";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> A[i];
+	}
+	int min = *min_element(A.begin(), A.end());
+	cout << "The minimum element of the array is " << min << " and its ordinal number is " << distance(A.begin(), min_element(A.begin(), A.end())) + 1 << endl;
 
 	return 0;
 }
